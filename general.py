f7a35c99d576ab9d04986c906ad4e9d6c17e09f5
Bhushan Hiwase <bhushiwase@gmail.com>
2020-06-26 02:01:27 -0700

    Update general.py

--- general.py
+++ general.py
@@ -1,6 +1,6 @@
 import textwrap
 
-# Returns MAC as string from bytes (ie AA:BB:CC:DD:EE:FF)
+# Returns MAC as string from the bytes (ie AA:BB:CC:DD:EE:FF)
 def get_mac_addr(mac_raw):
     byte_str = map('{:02x}'.format, mac_raw)
     mac_addr = ':'.join(byte_str).upper()
